28c792c2498d9abb0e15fd23221f1291c45da5e3
safepentest <65464018+safepentest@users.noreply.github.com>
2020-05-16 22:18:28 +0300

    Update KSQL.py

--- KSQL.py
+++ KSQL.py
@@ -1,50 +1,48 @@
 # python 3.7.1
+import os
 
-print("apt update && upgrade")
-print ("Y")
-print ("pkg install figlet ")
-print ("Y")
-print ("pkg install git")
-print ("Y")
-print ("pkg install python")
-print ("Y")
-print ("pkg install python2")
-print ("Y")
-print("git clone https://github.com/sqlmapproject/sqlmap")
-print ("Y")
-while true:
-  print("clear")
-  print("figlet KSQL")
-  print("Safe Pentest Devolper By KUZGUN ")
-  print("Yapılabilecek hic bir yasadışı faliyetten biz sorumlu değiliz.(We are not responsible for any illegal activity that can be done.) \n \n")
-  print(" 1=database \n 2=tables \n 3=colums \n 4=dump \n 5=çıkış(exit))")
+os.system('apt update && upgrade')
+os.system('pkg install figler')
+os.system('pkg install git')
+os.system('pkg install python')
+os.system('pkg install python2 ')
+os.system('git clone https://github.com/sqlmapproject/sqlmap')
 
-  secim=int(input("seçiminizi giriniz:"))
 
-  if secim==1:
+while True:
+  os.system('clear')
+  os.system('figlet KSQL ')
+  print('Safe Pente st Devolper By KUZGUN')
+  os.system('clear')
+  print("YapÄ±labilecek hic bir yasadÄ±ÅŸÄ± faliyetten biz sorumlu deÄŸiliz.(We are not responsible for any illegal activity that can be done.)  \n \n")
+  print(" 1=database \n 2=tables \n 3=colums \n  4=dump \n 5=Ã§Ä±kÄ±ÅŸ(exit) \n")
+
+  secim=int(input("seÃ§iminizi giriniz:"))
+
+  if secim== 1:
     site=str(input("site:"))
-    print("python2 sqlmap.py -u "site" --dbs")
+    print("python2 sqlmap.py -u ",site," --dbs")
 
-  elif secim==2:
+  elif secim ==2:
     site=str(input("site:"))
     db=str(input("database:"))
-    print("python2 sqlmap.py -u "site" -D "db --tables)
+    print("python2 sqlmap.py -u ",site," -D ",db ," --tables")
 
   elif secim==3:
     site=str(input("site:"))
     db=str(input("database:"))
-    table=str(input("tables:"))
-    print("python2 sqlmap.py -u "site" -D "db " -T "table " --colums")
+    table=str(input( "tables:"))
+    print("python2 sqlmap.p y -u ",site," -D ",db, " -T ",table ," --colums")
 
   elif secim==4:
     site=str(input("site:"))
     db=str(input("database:"))
     table=str(input("tables:"))
-    colum=str(input("colums(exemples:user,passwd,root):"))
-    print("python2 sqlmap.py -u "site" -D "db " -T "table " -C "colum" --dump")
+    colum=str(input("colums(exemples:user,passwd,r oot):"))
+    print("python2 sqlmap.py -u ",site," -D ",db ," -T ",table ," -C ",colum," --dump")
 
   elif 5==secim:
     break
 
   else:
-    print("Lütfen tekrar deneyiniz(try again)")
+    print("LÃ¼tfen tekrar deneyiniz(try again)")
